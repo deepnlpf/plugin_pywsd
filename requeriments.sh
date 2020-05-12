4ea52346a77187cebfbe261d9dcd0b375fa838f3
DylanVsn <43576618+DylanVsn@users.noreply.github.com>
2020-05-12 13:18:31 +0200

    install pywsd resources

--- requeriments.sh
+++ requeriments.sh
@@ -1,2 +1,8 @@
 # Install PyWSD
 pip install -U pywsd
+echo "Downloading resources..."
+python <<< "import nltk
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('punkt')
+"
